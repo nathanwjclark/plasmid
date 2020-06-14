c74ba0b446d06ba243e8c6dbb8865d333d3054cc
Nathan Clark <nathanclark@Nathan.C-MBPro>
2020-06-14 12:31:47 -0400

    Updates

---
+++ precache-manifest.80aaa09473d8a671a3263aac55927932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46dd4bcd508d9e84a1d77c4d233d6cb7",
+    "url": "/plasmid/index.html"
+  },
+  {
+    "revision": "9415e1297fcd35daeca3",
+    "url": "/plasmid/static/css/main.d1b943e7.chunk.css"
+  },
+  {
+    "revision": "625ba16f723b01034ded",
+    "url": "/plasmid/static/js/2.9cdbd5e1.chunk.js"
+  },
+  {
+    "revision": "61792b2604ba4eb5b8f5cfb89d5fed98",
+    "url": "/plasmid/static/js/2.9cdbd5e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9415e1297fcd35daeca3",
+    "url": "/plasmid/static/js/main.7cf1fb06.chunk.js"
+  },
+  {
+    "revision": "27a81273e5bb568001c4",
+    "url": "/plasmid/static/js/runtime-main.c28abb7f.js"
+  }
+]);
